7047f55b9394c54da805afe86a6a5e0cefd9a0a2
AbdurRahman11072 <rjrahman014@gmail.com>
2025-11-18 19:51:58 +0600

    remove all the console log

--- solution.ts
+++ solution.ts
@@ -61,7 +61,6 @@ const printBookDetails = (myBook: Book) => {
   const book = `Title: ${myBook.title}, Author: ${myBook.author}, Published: ${myBook.publishedYear}, Available: ${myBook.isAvailable}`;
   console.log(book);
 };
-//* Q7
 
 const getUniqueValues = (
   arr1: string[] | number[],
@@ -99,54 +98,3 @@ const calculateTotalPrice = (products: Product[]) => {
     return total + product.price * product.quantity;
   }, 0);
 };
-console.log("---------------------");
-
-console.log(formatValue("hello"));
-console.log(formatValue(5));
-console.log(formatValue(true));
-console.log("---------------------");
-console.log(getLength("typescript"));
-console.log(getLength([10, 20, 30, 40]));
-console.log("---------------------");
-const person1 = new Person("John Doe", 30);
-console.log(person1.getDetails());
-
-const person2 = new Person("Alice", 25);
-console.log(person2.getDetails());
-console.log("---------------------");
-const books = [
-  { title: "Book A", rating: 4.5 },
-  { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 },
-];
-
-console.log(filterByRating(books));
-console.log("---------------------");
-const users = [
-  { id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
-  { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
-  { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true },
-];
-
-console.log(filterActiveUsers(users));
-console.log("---------------------");
-const myBook: Book = {
-  title: "The Great Gatsby",
-  author: "F. Scott Fitzgerald",
-  publishedYear: 1925,
-  isAvailable: true,
-};
-
-printBookDetails(myBook);
-console.log("---------------------");
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-console.log(getUniqueValues(array1, array2));
-console.log("---------------------");
-const products = [
-  { name: "Pen", price: 10, quantity: 2 },
-  { name: "Notebook", price: 25, quantity: 3, discount: 10 },
-  { name: "Bag", price: 50, quantity: 1, discount: 20 },
-];
-
-console.log(calculateTotalPrice(products));
